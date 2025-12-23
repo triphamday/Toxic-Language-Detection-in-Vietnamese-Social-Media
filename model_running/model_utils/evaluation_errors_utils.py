b16186a319ac83a8ae97002779e0bbb8b3eab822
uynlu <uyenluubao2004@gmail.com>
2025-12-23 19:10:01 +0700

    Update error utils

--- model_running/model_utils/evaluation_errors_utils.py
+++ model_running/model_utils/evaluation_errors_utils.py
@@ -10,11 +10,11 @@ def error(labels, predictions, num_labels=2):
     acc = accuracy_score(labels, predictions)
     if num_labels == 2:
         f1 = f1_score(labels, predictions)
-        precision = precision_score(labels, predictions)
-        recall = recall_score(labels, predictions)
+        precision = precision_score(labels, predictions, zero_division=0)
+        recall = recall_score(labels, predictions, zero_division=0)
     else:
         f1 = f1_score(labels, predictions, average="macro")
-        precision = precision_score(labels, predictions, average="macro")
-        recall = recall_score(labels, predictions, average="macro")
+        precision = precision_score(labels, predictions, average="macro", zero_division=0)
+        recall = recall_score(labels, predictions, average="macro", zero_division=0)
     
     return acc, f1, precision, recall
